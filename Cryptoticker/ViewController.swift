cf1a2f424147de3b64621a986376a72d6cdbdbce
Dani <daniel.cr.ht@gmail.com>
2017-09-07 12:51:51 +0200

    Prepare the app to use CoreData

--- Cryptoticker/ViewController.swift
+++ Cryptoticker/ViewController.swift
@@ -7,20 +7,74 @@
 //
 
 import Cocoa
+import CoreData
 
 class ViewController: NSViewController {
 
   @IBOutlet weak var updateBtn: NSButton!
-  @IBOutlet weak var total: NSTextFieldCell!
   @IBOutlet weak var invested: NSTextFieldCell!
   @IBOutlet weak var btc: NSTextFieldCell!
   @IBOutlet weak var eth: NSTextFieldCell!
-  @IBOutlet weak var balance: NSTextFieldCell!
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
+
+    let appDelegate: AppDelegate = NSApplication.shared().delegate as! AppDelegate
+    let moc = appDelegate.managedObjectContext
+
+    // Initialize Fetch Request
+    let fetchRequest = NSFetchRequest<NSFetchRequestResult>()
+
+    // Create Entity Description
+    let entityDescription = NSEntityDescription.entity(forEntityName: "Balance", in: moc)
+
+    // Configure Fetch Request
+    fetchRequest.entity = entityDescription
+
+    var result: [Any] = [] // yes, the results are Any, not NSManagedObject
+    do {
+      result = try moc.fetch(fetchRequest)
+
+    } catch {
+      let fetchError = error as NSError
+      print(fetchError)
+    }
+
+    //let person = result[0] as! NSManagedObject
+
+    for balance in result {
+      moc.delete(balance as! NSManagedObject)
+
+      do {
+        try moc.save()
+      } catch {
+        let saveError = error as NSError
+        print(saveError)
+      }
+    }
+
+
+    let balance = NSManagedObject(entity: entityDescription!, insertInto: moc)
+    balance.setValue("BTC", forKey: "currency")
+    balance.setValue(1.0, forKey: "amount")
+    do {
+      try balance.managedObjectContext?.save()
+    } catch {
+      print(error)
+    }
+
+    do {
+      let result = try moc.fetch(fetchRequest)
+      print(result)
+
+    } catch {
+      let fetchError = error as NSError
+      print(fetchError)
+    }
+print("all done")
+
   }
 
   override var representedObject: Any? {
@@ -31,22 +85,9 @@ class ViewController: NSViewController {
 
   @IBAction func updateClicked(_ sender: Any) {
 
-    let rates = Rates()
-    rates.fetch(callback: self.refreshTotal)
+    let btc = Double(self.btc.title) ?? 0.0
+    let eth = Double(self.eth.title) ?? 0.0
 
-  }
-
-  func refreshTotal(rates: Rates) {
-    DispatchQueue.main.async {
-      let btc = Double(self.btc.title) ?? 0.0
-      let eth = Double(self.eth.title) ?? 0.0
-
-      let total = btc * (rates.eur["BTC"] ?? 0.0) + eth * (rates.eur["ETH"] ?? 0.0)
-      self.total.title = String(format: "%.2f", total)
-
-      let invested = Double(self.invested.title) ?? 0.0
-      self.balance.title = "Delta: " + String(format: "%.2f", total - invested)
-    }
   }
 }
 
